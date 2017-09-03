3b4f625bbad39620f1add664452c93ec996ea8bf
Brad House <brad@monetra.com>
2017-09-02 21:35:31 -0400

    import doxygen

---
+++ group__m__io__bluetooth.js
@@ -0,0 +1,11 @@
+var group__m__io__bluetooth =
+[
+    [ "M_io_bluetooth_enum_t", "group__m__io__bluetooth.html#ga74fcce28147c774e0e144a7902404756", null ],
+    [ "M_io_bluetooth_enum", "group__m__io__bluetooth.html#ga022d7bb5e89f1b3f36a9ec1d94308f34", null ],
+    [ "M_io_bluetooth_enum_destroy", "group__m__io__bluetooth.html#ga1c9fddbadb6bb61610fa2725a42ca11e", null ],
+    [ "M_io_bluetooth_enum_count", "group__m__io__bluetooth.html#ga4f800632a88591f2988df1e122d90575", null ],
+    [ "M_io_bluetooth_enum_name", "group__m__io__bluetooth.html#ga91ba8d0d3d4f33dedf5424bffcc39ede", null ],
+    [ "M_io_bluetooth_enum_mac", "group__m__io__bluetooth.html#gadd0ad00bb507b904909f6ee71c89eaf6", null ],
+    [ "M_io_bluetooth_enum_uuid", "group__m__io__bluetooth.html#gae3f5931c9384d297fca85d35446a5a1e", null ],
+    [ "M_io_bluetooth_create", "group__m__io__bluetooth.html#ga9ac2d422d79aa588a573f63c9e1de9d9", null ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,35 @@
+var NAVTREE =
+[
+  [ "Mstdlib-1.0.0", "index.html", [
+    [ "About", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__m__decimal.html#gac8dff6bf82fd9592d4d4d1b28826b8d8",
+"group__m__fs__dir.html#ga61c4c12df95c93de016d30d97dc1f07c",
+"group__m__hash__stridx.html#gad22c3acccffebad1bec70c22ff2c778e",
+"group__m__ini.html#ggaba7b134a1d375a874080b7b20e0ba727aba44e6da17c59cda0466f32e658885d2",
+"group__m__io__serial.html#ggaf5f6fa562b6f6be2d395ddcee1d36e28a29a1e095b3b9c8c03e7c72f5a9627bfe",
+"group__m__list__str.html#gabc4330f6755c84a077b8d5e281c32fc8",
+"group__m__log__common.html#gae8930caa6ff473862599faf5002ffece",
+"group__m__popen.html",
+"group__m__sql__driver.html#ggaf46c619c3bb9dfc5b9517537667ea911ab4b5df4f5a2bfc044dc53740f93de9fb",
+"group__m__state__machine.html#ga937c7ffbf381aea7b0117bb2f9b7ba0a",
+"group__m__thread__common__mutex.html#gac2c60a91c043e4fd15996511298d58b5",
+"group__m__xml.html#ga0d5e4aefea0d83fee84469e555cc049a"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
